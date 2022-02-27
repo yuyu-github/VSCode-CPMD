3ab5eaa3af52921fca2b156903a08988ce019771
yuyu <hcsd7b34@gmail.com>
2022-02-27 20:27:51 +0900

    Add: meta情報の自動補完を実装

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,30 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
+class CpmdCompletionItemProvider {
+	completionList: vscode.CompletionItem[] = [];;
 
+	constructor() {
+		const words = ['Version', 'UseImport', 'UseFileImport', 'UseWebImport', 'TypeToImport', 'RequiredVar', 'Var']
+		for (let word of words) {
+			this.completionList.push({
+				label: '#' + word,
+				kind: vscode.CompletionItemKind.Keyword,
+				insertText: word,
+			});
+		}
+	}
+
+	provideCompletionItems() {
+		return this.completionList;
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.languages.registerCompletionItemProvider(
+			'cpmd', new CpmdCompletionItemProvider(), '#'
+		)
+	);
 }
 
 export function deactivate() {}
